5605ceb037cfa92a017f7fa26fe4c36795b16b90
Akshat Zala <30282789+akshatz@users.noreply.github.com>
2019-01-29 14:39:01 +0530

    Revised with file not found exception
    File not found Exception Resolved

--- GuessTheMovie.java
+++ GuessTheMovie.java
@@ -5,30 +5,33 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class GuessTheMovie {
 
-    public static void main(String[] args) {
+    private static File file;
+
+    public static void main(String[] args) throws FileNotFoundException{
         Game game = new Game();
+        File file = new File("C:\\Users\\User2\\Desktop\\ud282-master\\GuesstheMovie\\src\\com\\movies.txt");
+        if(!file.exists()){
+            System.err.println("ERROR! file containing movielist is missing. Please input appropriate file and rerun.");
+            System.exit(0);
+        }
         System.out.println("Welcome to Guess the Movie!");
-        System.out.println("The rules are simple, the computer randomly picks a movie title, and shows you how " + "many " + "letters it's made up of.");
+        System.out.println("The rules are simple, the computer randomly picks a movie title, and shows you how " + "many " + "characters it's made up of. Please enter special charcterd like numbers,"+"!,"+"',"+" | "+" if you feel.");
         System.out.println("Your goal is to try to figure out the movie by guessing one letter at a time.");
-        System.out.println("If a letter is indeed in the title the computer will reveal its correct position in" + " the " + "word.");
-        File file = new File("movies.txt");
+        System.out.println("If a character is indeed in the title the computer will reveal its correct position in" + " the " + "word.");
+
 outer:
         while (true) {
-            String movieName = null;
-            try {
-                movieName = game.getRandomMovie(file);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
+            String movieName;
+            movieName = game.getRandomMovie(file);
             String cryptedString = game.hideString(movieName);
-            String y = null;
+            String y;
             int count = 0;
             char[] movieLetters;
             assert movieName != null;
             movieLetters = movieName.toCharArray();
-            System.out.println("This movie contains " + movieLetters.length + " letters");
+            System.out.println("This movie contains " + movieLetters.length + " characters");
             System.out.println("");
-            System.out.println("Enter a letter. You have 10 guesses left.");
+            System.out.println("Enter a character. You have 10 guesses left.");
             System.out.println(cryptedString);
             Scanner scanner = new Scanner(System.in);
             char[] correctLetters = cryptedString.toCharArray();
@@ -38,26 +41,28 @@ outer:
                 s = scanner.nextLine();
                 s = s.toLowerCase();
                 if (!(repeated.isEmpty()))
-                    System.out.println("\n" + "Please enter a letter");
+                    System.out.println("\n" + "Please enter a character");
                 {
-                    if (repeated.contains(s.substring(0))) {
-                        System.out.println("Letter is repeated. Please enter another letter.");
+                    if (repeated.contains(s)) {
+                        System.err.println("Character is repeated. Please enter another character.");
                         continue;
                     }
                 }
-                String nameGuessed = null;
-                if (!s.matches("[A-Za-z]")) {
-                    System.out.println("Please enter single letter only ");
-                } else {
+                String nameGuessed;
+                if (!s.matches("[A-Za-z0-9':-|!,.]"))
+                {
+                    System.err.println("Please enter single character only ");
+                }
+                else {
                     int i = 0;
                     i = movieName.indexOf(s);
                     char ch = 0;
                     try {
                         ch = s.charAt(0);
                     } catch (Exception ex) {
-                        System.out.println("Please enter only letter");
+                        System.err.println("Please enter characters, and numbers");
                     }
-                    int x = 0;
+                    int x;
                     if (i >= 0) {
                         for (x = 0; x < movieLetters.length; x++) {
                             if (movieLetters[x] == ch) {
@@ -77,13 +82,13 @@ outer:
                     } else {
                         count++;
                         if (count == 10) {
-                            System.out.println("Out of guesses, GAME OVER!");
-                            System.out.println("Correct movie is " + movieName.toUpperCase());
+                            System.err.println("Out of guesses, GAME OVER!");
+                            System.err.println("Correct movie is " + movieName.toUpperCase());
                             repeated.add(s);
                             continue;
                         } else {
                             int remain = 10 - count;
-                            System.out.println("WRONG! You have guessed " + count + " wrong letters!  try again. You have " + remain + " guesses left");
+                            System.err.println("WRONG! You have guessed " + count + " wrong letters!  try again. You have " + remain + " guesses left");
                             nameGuessed = new String(correctLetters);
                             System.out.println(nameGuessed);
                             repeated.add(s);
@@ -94,8 +99,7 @@ outer:
             System.out.println("Would you like to continue ? Y/N");
             y = scanner.nextLine();
             y = y.toLowerCase();
-            System.out.println(y);
-            if (y.substring(0).equals("y") || y.substring(0).equals("Y")) {
+            if (y.equals("y") || y.equals("Y")) {
                 continue outer;
             } else {
                 System.out.println("Thanks for playing");
